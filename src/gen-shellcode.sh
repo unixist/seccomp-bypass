3bdbf67efb4c41432f512a9e15ed1b66ce6582bf
Sean Williams <unixed@gmail.com>
2017-05-12 17:35:27 -0700

    don't need to generate binary

--- src/gen-shellcode.sh
+++ src/gen-shellcode.sh
@@ -3,4 +3,4 @@
 # Assume that first param is name of assembly file, e.g. foo.s. It expects it
 # to contain single code label called, "_start".
 file=$(sed "s/.s$//" <<< $1)
-as $file.s -o $file.o ; ld $file.o -o $file; objdump -D $file | grep -A10000 '<_start>' | sed -e 's/.*\t\([[:alnum:]].*\)\t.*/\1/' | cut -f2- -d: | sed ':a;/$/{N;s/\n//;ba}' | sed 's/^/\\x/' | sed -e 's/[[:space:]]\+\([[:alnum:]]\)/\\x\1/g'
+as $file.s -o $file.o  objdump -D $file.o | grep -A10000 '<_start>' | sed -e 's/.*\t\([[:alnum:]].*\)\t.*/\1/' | cut -f2- -d: | sed ':a;/$/{N;s/\n//;ba}' | sed 's/^/\\x/' | sed -e 's/[[:space:]]\+\([[:alnum:]]\)/\\x\1/g'
