590e71ff0e7bc0dc7bf42ed91139c7a36f59a903
Sean Williams <unixed@gmail.com>
2017-05-15 15:07:22 -0700

    Nicely format shellcode in .c file

--- gen-shellcode.sh
+++ gen-shellcode.sh
@@ -3,13 +3,13 @@
 # Assume that first param is name of assembly file, e.g. foo.s. It expects it
 # to contain single code label called, "_start".
 file=$(sed "s/.s$//" <<< $1)
-sc=$(as $file.s -o $file.o; objdump -D $file.o | grep -A10000 '<_start>' | sed -e 's/.*\t\([[:alnum:]].*\)\t.*/\1/' | cut -f2- -d: | sed ':a;/$/{N;s/\n//;ba}' | sed 's/^/\\x/' | sed -e 's/[[:space:]]\+\([[:alnum:]]\)/\\x\1/g' | sed -e 's/[[:space:]]\+$//')
+sc=$(as $file.s -o $file.o; objdump -D $file.o | grep -A10000 '<_start>' | sed -e 's/.*\t\([[:alnum:]].*\)\t.*/\1/' | cut -f2- -d: | sed ':a;/$/{N;s/\n//;ba}' | sed 's/^/\\x/' | sed -e 's/[[:space:]]\+\([[:alnum:]]\)/\\x\1/g' | sed -e 's/[[:space:]]\+$//' | sed 's/\\x/\n/g' | column -c 120 -x | sed 's/^/\\x/; s/\t/\\x/g' | sed '1 s/^/"/;1 s/$/"/; 2,$ s/^\(.*\)/                   "\1"/')
 rm $file.o
 cat << EOF
 #include <stdint.h>
 #include <sys/mman.h>
 
-char shellcode[] = "$sc";
+char shellcode[] = $sc;
 
 int main(){
   mprotect((void *)((uint64_t)shellcode & ~4095), 4096, PROT_READ|PROT_EXEC);
@@ -17,4 +17,3 @@ int main(){
   return 0;
 }
 EOF
-> /tmp/b
