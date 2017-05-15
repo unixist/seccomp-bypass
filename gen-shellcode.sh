dc0e5208da563be0d4128b27ac1efd02ebda5c8e
Sean Williams <unixed@gmail.com>
2017-05-15 12:58:25 -0700

    Remove trailing space in generated shellcode

--- gen-shellcode.sh
+++ gen-shellcode.sh
@@ -3,7 +3,7 @@
 # Assume that first param is name of assembly file, e.g. foo.s. It expects it
 # to contain single code label called, "_start".
 file=$(sed "s/.s$//" <<< $1)
-sc=$(as $file.s -o $file.o; objdump -D $file.o | grep -A10000 '<_start>' | sed -e 's/.*\t\([[:alnum:]].*\)\t.*/\1/' | cut -f2- -d: | sed ':a;/$/{N;s/\n//;ba}' | sed 's/^/\\x/' | sed -e 's/[[:space:]]\+\([[:alnum:]]\)/\\x\1/g')
+sc=$(as $file.s -o $file.o; objdump -D $file.o | grep -A10000 '<_start>' | sed -e 's/.*\t\([[:alnum:]].*\)\t.*/\1/' | cut -f2- -d: | sed ':a;/$/{N;s/\n//;ba}' | sed 's/^/\\x/' | sed -e 's/[[:space:]]\+\([[:alnum:]]\)/\\x\1/g' | sed -e 's/[[:space:]]\+$//')
 rm $file.o
 cat << EOF
 #include <stdint.h>
