633b9ac2d4e6d6984257992c3adeed798c8fda64
Sean Williams <unixed@gmail.com>
2017-05-16 12:42:02 -0700

    Fix up comments

--- src/read-with-sendfile.s
+++ src/read-with-sendfile.s
@@ -1,12 +1,13 @@
 .globl _start
 _start:
-	movw $0x7374, %r11w            /* ts */
+	// open
+	movw $0x7374, %r11w             /* ts */
 	push %r11
 	movq $0x736f682f6374652f, %r11  /* /etc/hos */
 	push %r11
 	lea 0(%rsp), %rdi
 	xor %rsi, %rsi
-	addb $2, %al                   /* %rsi is initialized to 0 */
+	addb $2, %al
 	syscall
 
 	// sendfile
